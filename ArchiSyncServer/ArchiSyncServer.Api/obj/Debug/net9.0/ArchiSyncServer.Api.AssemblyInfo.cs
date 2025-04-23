b563a3ed4e25f99b5d27f6be2963ebc69c03c8e6
rutprogramming <rutprogramming8978@gmail.com>
2025-04-23 18:13:29 +0300

    docker

--- ArchiSyncServer/ArchiSyncServer.Api/obj/Debug/net9.0/ArchiSyncServer.Api.AssemblyInfo.cs
+++ ArchiSyncServer/ArchiSyncServer.Api/obj/Debug/net9.0/ArchiSyncServer.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArchiSyncServer.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c92ddb0e9d2c2054f9e5411f1d4536b3556d28b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8baad6235adcddaa9676fdc2540fbad06c457d74")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArchiSyncServer.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArchiSyncServer.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
